cebdb4cd49f80875300385bfcbf703741db97ca0
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-13 10:15:31 -0700

    write user_with_proficiency instance method in Skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,14 @@
 class Skill < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :proficiencies
+  has_many :users, through: :proficiencies
+  validates_uniqueness_of :name
+
+  def user_with_proficiency(num)
+    # we want to return a user object who has a proficiency of num for skill of self.
+    proficiency_results = self.proficiencies.where(proficiency: num) 
+    proficiency_object = proficiency_results.first
+    proficiency_object.user
+  end
+
 end
