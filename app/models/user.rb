fc9453fbf130c86728a134898a129f819de8e0d7
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-13 10:16:03 -0700

    add methods for getting, setting proficiencies in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :proficiencies
+  has_many :skills, through: :proficiencies
+
+  def proficiency_for(skill) # this is a skill object, not just it's name
+    proficiency_results = Proficiency.where(user_id: self.id, skill_id: skill.id)
+    proficiency_object = proficiency_results.first
+    proficiency_object.proficiency
+  end
+
+  def set_proficiency_for(skill, value)
+    current_proficiency = Proficiency.where(skill_id: skill.id, user_id: self.id).first
+    current_proficiency.proficiency = value
+    current_proficiency.save
+  end
 end
