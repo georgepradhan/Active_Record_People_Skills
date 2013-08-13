5d4bfdb9b65859f508ed55d8cd06270327254400
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-13 10:45:32 -0700

    update User#proficiency_for(skill) method to return something if the user does not have the skill."

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :skills, through: :proficiencies
 
   def proficiency_for(skill) # this is a skill object, not just it's name
+    return "Does not have this skill" unless self.skills.include?(skill)
     proficiency_results = Proficiency.where(user_id: self.id, skill_id: skill.id)
     proficiency_object = proficiency_results.first
     proficiency_object.proficiency
